d5fabd0879e0a23c9e24ed4f811e1ab3da1f68ce
rishsa <ekatesa144@gmail.com>
2022-08-02 13:36:28 +0400

    feat: solution found

--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ module.exports = function check(str, bracketsConfig) {
   openm = []
   closem = []
   for(n in bracketsConfig){
-      //openm.push(n+bracketsConfig[n][0])
       openm.push(n)
       openm.push(bracketsConfig[n][0])
-      //closem.push(n+bracketsConfig[n][1])
       closem.push(n)
       closem.push(bracketsConfig[n][1])
   }
-  //console.log(openm, closem)
   rez = []
   for(s of str) {
-      if (rez.length<1 || openm.indexOf(s)>-1) rez.push(s)
-      else if (openm.indexOf(rez.at(-1))-1 == closem.indexOf(s)-1) rez.pop()
-     //rez.at(-1)
+      if (rez.length<1) {rez.push(s); continue}
+      if (openm.indexOf(rez.at(-1))-1 == closem.indexOf(s)-1 && closem.indexOf(s)>-1) rez.pop()
+      else rez.push(s)
   }
-  // your solution
   return (rez.length > 0) ? false : true
 }
